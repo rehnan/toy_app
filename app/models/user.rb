7feaed4d19216497624cd188d434ae5353037c28
Rehnan Carolino <rehnancarolino@live.com>
2015-04-07 22:10:17 -0300

    validation to email attribute by REGEX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	validates :email, :name, presence: true 
 	validates :email, uniqueness: true
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :on => :create
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :update
 	has_many :microposts
 end
