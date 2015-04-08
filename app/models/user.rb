d2731d954777bb7fa083ff8b0125fd980e500174
Rehnan Carolino <rehnancarolino@live.com>
2015-04-08 08:40:27 -0300

    bugfix validation to email attribute by REGEX to Update and Create actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
 	validates :email, :name, presence: true 
 	validates :email, uniqueness: true
-	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :on => :create
-	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :update
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :on => [ :create, :update ]
 	has_many :microposts
 end
